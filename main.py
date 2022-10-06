5315c25f8d861fd3fe52352da27d063a6a17b722
Arij Ben Rhouma <arij.benrhouma@student-cs.fr>
2022-10-06 11:23:53 +0200

    trafficdata

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
 df1 = pd.read_csv ('consolidation-etalab-schema-irve-v-2.0.3-20221004.csv')
 df2 = pd.read_csv('tmja-2019.csv', sep=";")
-print(df2.keys())
+#print(df2.keys())
+#print(df2['TMJA'])
+df2.to_excel('trafficdata.xlsx')
